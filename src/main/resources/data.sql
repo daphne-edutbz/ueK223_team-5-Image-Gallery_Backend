736238f47786c7ab6bb0319c6fc2e4e91c602133
Gino Wenger <gino.wenger@edu.tbz.ch>
2026-01-15 14:11:32 +0100

    Add new seed users and remove DEFAULT role in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,15 +4,22 @@
 
 
 insert into users (id, email,first_name,last_name, password)
-values ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'admin@example.com', 'James','Bond', '$2a$10$TM3PAYG3b.H98cbRrHqWa.BM7YyCqV92e/kUTBfj85AjayxGZU7d6' ), -- Password: 1234
-('0d8fa44c-54fd-4cd0-ace9-2a7da57992de', 'user@example.com', 'Tyler','Durden', '$2a$10$TM3PAYG3b.H98cbRrHqWa.BM7YyCqV92e/kUTBfj85AjayxGZU7d6') -- Password: 1234
+values ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'admin@example.com', 'Gianluca','Daffré', '$2a$10$TM3PAYG3b.H98cbRrHqWa.BM7YyCqV92e/kUTBfj85AjayxGZU7d6' ), -- Password: 1234
+('0d8fa44c-54fd-4cd0-ace9-2a7da57992de', 'olivia.parker@example.com', 'Olivia','Parker', '$2a$10$TM3PAYG3b.H98cbRrHqWa.BM7YyCqV92e/kUTBfj85AjayxGZU7d6'), -- Password: 1234
+('8f3dd8a1-7f1a-4c62-8cb5-2c55f9e6a4a1', 'daniel.brooks@example.com', 'Daniel','Brooks', '$2a$10$TM3PAYG3b.H98cbRrHqWa.BM7YyCqV92e/kUTBfj85AjayxGZU7d6'), -- Password: 1234
+('1b8f87e9-1c02-4a2d-92f8-5a52f7d2e3c4', 'mia.wallace@example.com', 'Mia','Wallace', '$2a$10$TM3PAYG3b.H98cbRrHqWa.BM7YyCqV92e/kUTBfj85AjayxGZU7d6'), -- Password: 1234
+('9b2f6c1e-9f3a-4d2b-8ad1-5c2a45d6f8a0', 'emma.wilson@example.com', 'Emma','Wilson', '$2a$10$TM3PAYG3b.H98cbRrHqWa.BM7YyCqV92e/kUTBfj85AjayxGZU7d6'), -- Password: 1234
+('4c1a5f88-2d8f-4e6b-9b8f-8e1f1c0b2a31', 'liam.hughes@example.com', 'Liam','Hughes', '$2a$10$TM3PAYG3b.H98cbRrHqWa.BM7YyCqV92e/kUTBfj85AjayxGZU7d6'), -- Password: 1234
+('6a0e2d5c-7b92-46d2-9d8e-1c2b9f8a4d10', 'sophia.carter@example.com', 'Sophia','Carter', '$2a$10$TM3PAYG3b.H98cbRrHqWa.BM7YyCqV92e/kUTBfj85AjayxGZU7d6'), -- Password: 1234
+('a3d7c1b0-5e24-4a95-8f3c-7c2a1b4d9e6f', 'jackson.lee@example.com', 'Jackson','Lee', '$2a$10$TM3PAYG3b.H98cbRrHqWa.BM7YyCqV92e/kUTBfj85AjayxGZU7d6'), -- Password: 1234
+('f7c2b4a1-3d6e-4b2f-9a1c-5e8d3b1a7c90', 'ava.morgan@example.com', 'Ava','Morgan', '$2a$10$TM3PAYG3b.H98cbRrHqWa.BM7YyCqV92e/kUTBfj85AjayxGZU7d6'), -- Password: 1234
+('2e8b6a4d-1f93-4c7b-9e2a-0d5f7c3a9b21', 'noah.reed@example.com', 'Noah','Reed', '$2a$10$TM3PAYG3b.H98cbRrHqWa.BM7YyCqV92e/kUTBfj85AjayxGZU7d6') -- Password: 1234
  ON CONFLICT DO NOTHING;
 
 
 --ROLES
 INSERT INTO role(id, name)
-VALUES ('d29e709c-0ff1-4f4c-a7ef-09f656c390f1', 'DEFAULT'),
-('ab505c92-7280-49fd-a7de-258e618df074', 'ADMIN'),
+VALUES ('ab505c92-7280-49fd-a7de-258e618df074', 'ADMIN'),
 ('c6aee32d-8c35-4481-8b3e-a876a39b0c02', 'USER')
 ON CONFLICT DO NOTHING;
 
@@ -25,15 +32,23 @@ ON CONFLICT DO NOTHING;
 
 --assign roles to users
 insert into users_role (users_id, role_id)
-values ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'd29e709c-0ff1-4f4c-a7ef-09f656c390f1'),
-       ('0d8fa44c-54fd-4cd0-ace9-2a7da57992de', 'd29e709c-0ff1-4f4c-a7ef-09f656c390f1'),
+values ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'c6aee32d-8c35-4481-8b3e-a876a39b0c02'),
+       ('0d8fa44c-54fd-4cd0-ace9-2a7da57992de', 'c6aee32d-8c35-4481-8b3e-a876a39b0c02'),
+       ('8f3dd8a1-7f1a-4c62-8cb5-2c55f9e6a4a1', 'c6aee32d-8c35-4481-8b3e-a876a39b0c02'),
+       ('1b8f87e9-1c02-4a2d-92f8-5a52f7d2e3c4', 'c6aee32d-8c35-4481-8b3e-a876a39b0c02'),
+       ('9b2f6c1e-9f3a-4d2b-8ad1-5c2a45d6f8a0', 'c6aee32d-8c35-4481-8b3e-a876a39b0c02'),
+       ('4c1a5f88-2d8f-4e6b-9b8f-8e1f1c0b2a31', 'c6aee32d-8c35-4481-8b3e-a876a39b0c02'),
+       ('6a0e2d5c-7b92-46d2-9d8e-1c2b9f8a4d10', 'c6aee32d-8c35-4481-8b3e-a876a39b0c02'),
+       ('a3d7c1b0-5e24-4a95-8f3c-7c2a1b4d9e6f', 'c6aee32d-8c35-4481-8b3e-a876a39b0c02'),
+       ('f7c2b4a1-3d6e-4b2f-9a1c-5e8d3b1a7c90', 'c6aee32d-8c35-4481-8b3e-a876a39b0c02'),
+       ('2e8b6a4d-1f93-4c7b-9e2a-0d5f7c3a9b21', 'c6aee32d-8c35-4481-8b3e-a876a39b0c02'),
        ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'ab505c92-7280-49fd-a7de-258e618df074'),
        ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'c6aee32d-8c35-4481-8b3e-a876a39b0c02')
  ON CONFLICT DO NOTHING;
 
 --assign authorities to roles
 INSERT INTO role_authority(role_id, authority_id)
-VALUES ('d29e709c-0ff1-4f4c-a7ef-09f656c390f1', '2ebf301e-6c61-4076-98e3-2a38b31daf86'),
-('ab505c92-7280-49fd-a7de-258e618df074', '76d2cbf6-5845-470e-ad5f-2edb9e09a868'),
+VALUES ('ab505c92-7280-49fd-a7de-258e618df074', '76d2cbf6-5845-470e-ad5f-2edb9e09a868'),
+('c6aee32d-8c35-4481-8b3e-a876a39b0c02', '2ebf301e-6c61-4076-98e3-2a38b31daf86'),
 ('c6aee32d-8c35-4481-8b3e-a876a39b0c02', '21c942db-a275-43f8-bdd6-d048c21bf5ab')
  ON CONFLICT DO NOTHING;

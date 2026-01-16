60f318008069a024086e6b7cc8eae80b135c937e
David-Tarlos <david.tarlos@edu.tbz.ch>
2026-01-16 09:06:14 +0100

    add @Service to ImagePostService

--- src/main/java/com/example/demo/domain/imagepost/ImagePostService.java
+++ src/main/java/com/example/demo/domain/imagepost/ImagePostService.java
@@ -2,9 +2,10 @@ package com.example.demo.domain.imagepost;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
-
+@Service
 public interface ImagePostService {
 
     ImagePost create(ImagePost post);

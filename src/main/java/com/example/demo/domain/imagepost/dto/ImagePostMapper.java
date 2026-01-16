7f3ef09639785859bdebcebdb6e6c49fcb3811c0
Gino Wenger <gino.wenger@edu.tbz.ch>
2026-01-16 14:14:53 +0100

    map image post author id in dto
    include authorId and likeCount in ImagePostMapper

--- src/main/java/com/example/demo/domain/imagepost/dto/ImagePostMapper.java
+++ src/main/java/com/example/demo/domain/imagepost/dto/ImagePostMapper.java
@@ -3,9 +3,15 @@ package com.example.demo.domain.imagepost.dto;
 import com.example.demo.core.generic.AbstractMapper;
 import com.example.demo.domain.imagepost.ImagePost;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ImagePostMapper extends AbstractMapper<ImagePost, ImagePostDTO> {
+    @Override
+    @Mapping(target = "authorId", source = "author.id")
+    @Mapping(target = "likeCount", expression = "java(post.getLikedBy() == null ? 0 : post.getLikedBy().size())")
+    ImagePostDTO toDTO(ImagePost post);
+
     ImagePost fromCreateDTO(ImagePostCreateDTO dto);
 }
